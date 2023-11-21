0ac90d2f624dbf008d547211fd25eed656938301
Tomek Padzik <tomek.padzik@ingeniousio.com>
2023-11-21 09:37:10 +0200

    updated UserDataResource

--- app/Modules/Duel/Http/Resources/UserDataResource.php
+++ app/Modules/Duel/Http/Resources/UserDataResource.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Modules\Duel\Http\Resources;
 
+use App\Modules\Cards\App\Collection\CardsCollection;
 use App\Modules\Cards\App\Contracts\CardsFacadeInterface;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
@@ -11,22 +12,47 @@ use Illuminate\Support\Facades\App;
 
 class UserDataResource extends JsonResource
 {
-
     public function toArray(Request $request): array
     {
-        // @todo: get user cards Ids,
-        $cardsIds = [1,3,7,8,11,12];
-        
-        
         return [
             'id' => (int)$request->user()->getKey(),
             'username' => $request->user()->name,
-            'level' => 1,
-            'level_points' => '40/100',
-            'cards' => App::make(CardsFacadeInterface::class)
-                ->findIds(ids: $cardsIds),
-            'new_card_allowed' => true,
+            'level' => $this->level(),
+            'level_points' => $this->levelPoints(),
+            'cards' => $this->userCards()->toArray(),
+            'new_card_allowed' => $this->areNewCardsAllowed(),
         ];
+    }
 
+    private function level(): int
+    {
+        // @todo
+        return 1;
+    }
+
+    private function levelPoints(): string
+    {
+        // @todo
+        return '40/100';
+    }
+
+    private function userCards(): CardsCollection
+    {
+        return App::make(CardsFacadeInterface::class)
+            ->findIds(ids: $this->userCardsIds());
+    }
+
+
+    private function areNewCardsAllowed(): bool
+    {
+        // @todo
+        return true;
+    }
+
+
+    private function userCardsIds(): array
+    {
+        // @todo: get real user cards Ids,
+        return [1, 3, 7, 8, 11, 12];
     }
 }
